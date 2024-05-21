5cb4c9893ac27b86025ca4ba158a9fec073c57d4
Vuk Perovic <vukperovic03@gmail.com>
2024-05-21 19:38:43 +0200

    Refactored the last feature to a function and made it safer by locking the file.

--- main.c
+++ main.c
@@ -2,13 +2,26 @@
 #include <unistd.h>
 #include <mqueue.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/file.h>
+
+#define PROC_FILE "processes.txt"
+#define MAX_USERS 4
+
+void sign_in();
 
 int main(void) {
-    // Append the process ID to a file
-    pid_t pid = getpid();
-    FILE *file = fopen("processes.txt", "a");
-    fprintf(file, "%d\n", pid);
-    fclose(file);
+    sign_in();
 
     return 0;
+}
+
+void sign_in() {
+    pid_t pid = getpid();
+    int proc_file = open(PROC_FILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    flock(proc_file, LOCK_EX);
+    dprintf(proc_file, "%d\n", pid);
+    flock(proc_file, LOCK_UN);
+    close(proc_file);
 }
