cf2e95314ef1740cd6adcf39f9e5f0dbb144d909
Vuk Perovic <vukperovic03@gmail.com>
2024-05-21 17:09:18 +0200

    Modified .gitignore and added the functionality that writes the pid to a file.

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <mqueue.h>
+#include <pthread.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    // Append the process ID to a file
+    pid_t pid = getpid();
+    FILE *file = fopen("processes.txt", "a");
+    fprintf(file, "%d\n", pid);
+    fclose(file);
+
     return 0;
 }
