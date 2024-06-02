4412256ddef447c59d5dd050ade438d1b83f54f7
Vuk Perovic <vukperovic03@gmail.com>
2024-06-02 21:53:49 +0200

    Removed the listening thread, added functions for queues.
    The process now cannot register processes that joined after the timeout. Added initialize(), open_queues(), cleanup() functions and variables queue_names and queues.

--- main.c
+++ main.c
@@ -11,36 +11,41 @@
 
 #define PROC_FILE "processes.txt"
 #define MAX_USERS 4
+#define TIMEOUT 5
+#define NAME_SIZE 16
 
 bool running = true;
 pid_t pid;
 int pids[MAX_USERS] = {0};
+char *queue_names[MAX_USERS];
+mqd_t queues[MAX_USERS];
+
+struct mq_attr attr;
 
 void sign_in(pid_t pid);
 
-void *update_pids(void *arg);
+void update_pids();
 
 void get_pids_from_file(int arr[]);
 
+void initialize();
+
+void open_queues();
+
+void cleanup();
+
 int main(void) {
-    pid = getpid();
-    pthread_t listening;
+    initialize();
+    open_queues();
 
-    sign_in(pid);
 
-    pthread_create(&listening, NULL, update_pids, NULL);
-    sleep(8);
-    running = false;
-    pthread_join(listening, NULL);
 
-    for (int i = 0; i < MAX_USERS; ++i) {
-        printf("%d\n", pids[i]);
-    }
-
+    cleanup();
     return 0;
 }
 
 void sign_in(pid_t pid) {
+    // Writes the process's ID to the processes.txt file and asigns that value as the first element of pids[]
     pids[0] = pid;
     int proc_file = open(PROC_FILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
     flock(proc_file, LOCK_EX);
@@ -50,6 +55,7 @@ void sign_in(pid_t pid) {
 }
 
 void get_pids_from_file(int arr[]) {
+    // Writes pid's from processes.txt to the given array
     char big_buff[32];
     int i = 0;
 
@@ -73,19 +79,56 @@ void get_pids_from_file(int arr[]) {
     }
 }
 
-void *update_pids(void *arg) {
-    while (running) {
-        sleep(5);
-        int arr[MAX_USERS] = {0};
-        get_pids_from_file(arr);
+void update_pids() {
+    // Reads the pid's from processes.txt and updates the pids[] array accordingly
+    sleep(TIMEOUT);
+    int arr[MAX_USERS] = {0};
+    get_pids_from_file(arr);
 
-        int i = 1;
-        for (int j = 0; j < MAX_USERS; ++j) {
-            if (arr[j] == pid) {
-                continue;
-            }
-            pids[i++] = arr[j];
+    int i = 1;
+    for (int j = 0; j < MAX_USERS; ++j) {
+        if (arr[j] == pid) {
+            continue;
+        }
+        pids[i++] = arr[j];
+    }
+}
+
+void initialize() {
+    pid = getpid();
+    sign_in(pid);
+    update_pids();
+
+    // Allocate space for mq names and assign them to '/pid'
+    for (int i = 0; i < MAX_USERS; ++i) {
+        queue_names[i] = (char *) malloc(NAME_SIZE * sizeof(char));
+        if (pids[i] == 0) continue;
+        snprintf(queue_names[i], NAME_SIZE, "/%d", pids[i]);
+    }
+
+    // Initialize the mq_attr struct
+    attr.mq_msgsize = 256;
+    attr.mq_maxmsg = 8;
+    attr.mq_flags = 0;
+    attr.mq_curmsgs = 0;
+}
+
+void open_queues() {
+    for (int i = 0; i < MAX_USERS; ++i) {
+        if (pids[i] == 0) return;
+        if (i == 0) {   // Set up the process's receiving queue
+            queues[i] = mq_open(queue_names[i], O_RDONLY | O_CREAT, 0644, &attr);
+        } else {    // Set up the queues for sending messages
+            queues[i] = mq_open(queue_names[i], O_WRONLY | O_CREAT, 0644, &attr);
         }
     }
-    return NULL;
+}
+
+void cleanup() {
+    // Frees allocated memory, closes mq descriptors and removes their names
+    for (int i = 0; i < MAX_USERS; ++i) {
+        free(queue_names[i]);
+        mq_close(queues[i]);
+        mq_unlink(queue_names[i]);
+    }
 }
